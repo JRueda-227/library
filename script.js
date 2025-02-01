0bf9e115d52ae69badc33fe4287d883aaea63d5e
Your Juan Rueda <j.ruedavargas21@gmail.com>
2025-02-01 15:01:55 -0500

    Create Book Card Display with DOM

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function addBookToLibrary(title, author, pages) {
     myLibrary.push(newBook);
 }
 
+addBookToLibrary('LOTR', 'Tolkien', '295')
+addBookToLibrary('Meditations', 'Marcus Aurelious', '200');
+
+function displayBook() {
+    for (let book of myLibrary) {
+        const card = document.createElement('div');
+        const title = document.createElement('h3');
+        const author = document.createElement('h4');
+        const pages = document.createElement('h4');
+
+        card.classList.add('card');
+
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        author.textContent = book.author;
+        card.appendChild(author);
+
+        pages.textContent = book.pages;
+        card.appendChild(pages);
+
+        library.appendChild(card);
+    }
+}
+
+const library = document.querySelector('.library');
+
+const button = document.querySelector('button')
+button.addEventListener('click', displayBook);
