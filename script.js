ee95ffa4a912a2cc0efab50da7ca7416e02600fe
Your Juan Rueda <j.ruedavargas21@gmail.com>
2025-02-01 14:11:33 -0500

    Add JS Functions to Create Books and Add Them to Array

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+}
+
